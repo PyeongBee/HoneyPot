ea04bd6c7a74eb9082897b0a35def436987899e4
pyeongbee <pyeongbee0806@gmail.com>
2025-11-04 02:26:13 +0900

    문항 좌우 패딩 제거

--- src/app/(pages)/editor/_components/QuestionEditor.tsx
+++ src/app/(pages)/editor/_components/QuestionEditor.tsx
@@ -30,7 +30,7 @@ export default function QuestionEditor({
   return (
     <div
       className={clsx(
-        "px-6 py-4 bg-gray-50 dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700",
+        "py-4 bg-gray-50 dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700",
         containerClassName ?? "mt-20"
       )}
     >
